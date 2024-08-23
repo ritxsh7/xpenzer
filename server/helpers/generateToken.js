aa8ec0683bf081bb0a65876381bb49bae95762a8
Ritesh Patil <ritessshhh7@gmail.com>
2024-08-23 21:49:59 +0530

    Auth | added profile to token payload

--- server/helpers/generateToken.js
+++ server/helpers/generateToken.js
@@ -2,7 +2,12 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (isUser) => {
   const token = jwt.sign(
-    { userId: isUser.user_id, phone: isUser.phone, username: isUser.username },
+    {
+      userId: isUser.user_id,
+      phone: isUser.phone,
+      username: isUser.username,
+      profile: isUser.profile_color,
+    },
     process.env.JWT_SECRET,
     {
       expiresIn: "30d",
