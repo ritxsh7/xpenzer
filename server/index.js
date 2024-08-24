e1901c2807559f2e1d0f6684b6285b46d63fd479
Ritesh Patil <ritessshhh7@gmail.com>
2024-08-24 18:15:14 +0530

    Security | cors update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // EXPRESS APP
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
