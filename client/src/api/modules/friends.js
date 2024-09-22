7ebd845bd09d0f1f82cc629375cd76a63a8c1217
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-22 15:48:26 +0530

    api | search by phone number

--- client/src/api/modules/friends.js
+++ client/src/api/modules/friends.js
@@ -3,6 +3,7 @@ import { client } from "../client.js";
 const friendsEndpoints = {
   getAll: "/friends/all",
   getFriendLike: (name) => `/friends?like=${name}`,
+  getUsersLike: (name) => `/friends/users?like=${name}`,
 };
 
 const friendsApi = {
@@ -17,6 +18,14 @@ const friendsApi = {
   getFriendLike: async (name) => {
     try {
       const res = await client.get(friendsEndpoints.getFriendLike(name));
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getUsersLike: async (name) => {
+    try {
+      const res = await client.get(friendsEndpoints.getUsersLike(name));
       return res;
     } catch (error) {
       throw error;
