8072aaab18c546de4ea735ce9f901a627bd5de03
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-23 21:00:03 +0530

    ui | new friend success , error

--- client/src/api/modules/friends.js
+++ client/src/api/modules/friends.js
@@ -4,6 +4,7 @@ const friendsEndpoints = {
   getAll: "/friends/all",
   getFriendLike: (name) => `/friends?like=${name}`,
   getUsersLike: (name) => `/friends/users?like=${name}`,
+  newFriend: (fid) => `/friends/new?fid=${fid}`,
 };
 
 const friendsApi = {
@@ -26,6 +27,14 @@ const friendsApi = {
   getUsersLike: async (name) => {
     try {
       const res = await client.get(friendsEndpoints.getUsersLike(name));
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  },
+  newFriend: async (fid) => {
+    try {
+      const res = await client.post(friendsEndpoints.newFriend(fid));
       return res;
     } catch (error) {
       throw error;
