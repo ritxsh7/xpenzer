6bd8ba385eed1a7285efffd8cc1d212b48d0424d
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-26 21:35:45 +0530

    comps - dp and list

---
+++ client/src/components/groups/GroupDisplay.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AvatarComp from "../common/Avatar";
+
+const GroupDisplay = ({ group }) => {
+  return (
+    <>
+      <div className="flex items-center text-sm bg-[#1c1c1c] p-3 rounded-lg shadow">
+        <AvatarComp name={group.groupName} color={group.avatarColor} />
+        <div className="ml-4 text-left">
+          <p className="font-medium">{group.groupName}</p>
+          <p className=" text-gray-400">{group.lastActivity}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GroupDisplay;
---
+++ client/src/components/groups/GroupList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GroupDisplay from "./GroupDisplay";
+
+const GroupList = ({ groups }) => {
+  return (
+    <div className="space-y-4 mt-8">
+      {groups.map((group) => (
+        <GroupDisplay group={group} key={group.id} />
+      ))}
+    </div>
+  );
+};
+
+export default GroupList;
