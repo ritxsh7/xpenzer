5033882c20f70596331c9d21c6e28c0727a6f798
Ritesh Patil <ritessshhh7@gmail.com>
2024-09-30 23:47:01 +0530

    api - notis routing

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { autoLogin, loginUser, signupUser } from "../controller/users/users.js";
+import {
+  autoLogin,
+  getAllNotifications,
+  loginUser,
+  signupUser,
+} from "../controller/users/users.js";
 import { loginValidators, signupValidators } from "../helpers/validation.js";
 import { authenticate } from "../middleware/auth.js";
 
@@ -8,5 +13,6 @@ const userRoutes = Router();
 userRoutes.post("/user/signup", signupValidators, signupUser);
 userRoutes.post("/user/login", loginValidators, loginUser);
 userRoutes.get("/user/auth", authenticate, autoLogin);
+userRoutes.get("/notifications", authenticate, getAllNotifications);
 
 export default userRoutes;
