85555e2d035d2123c2016ff96f805d1bf68bf5b7
Ritesh <ritessshhh7@gmail.com>
2024-11-29 17:47:18 +0530

    implment download transactions functionality

--- client/src/components/friends/TransactionList.jsx
+++ client/src/components/friends/TransactionList.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import Transaction from "./Transaction";
+import { BsDownload } from "react-icons/bs";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import DownloadList from "../layout/DownloadList";
 
 const TransactionList = ({ transactions, setSettling }) => {
+  /* TransactionList comp here*/
+
+  //ref
+  const transactionListRef = useRef(null);
+
   return (
-    <div className="flex flex-col gap-3 my-6 mb-16">
-      {transactions.map((trx) => (
-        <Transaction item={trx} key={trx.contri_id} setSettling={setSettling} />
-      ))}
+    <div className="flex flex-col">
+      <div className="absolute right-8 top-[17.5rem]">
+        <PDFDownloadLink
+          document={<DownloadList transactions={transactions} />}
+          fileName="transactions.pdf"
+        >
+          <BsDownload />
+        </PDFDownloadLink>
+      </div>
+      <div className="flex flex-col gap-3 my-6 mb-16" ref={transactionListRef}>
+        {transactions.map((trx) => (
+          <Transaction
+            item={trx}
+            key={trx.contri_id}
+            setSettling={setSettling}
+          />
+        ))}
+      </div>
     </div>
   );
 };
